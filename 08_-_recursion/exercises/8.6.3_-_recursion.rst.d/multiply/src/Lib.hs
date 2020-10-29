ef712073dd72a91339f04c582d2d26b72f05c966
Chris King-Parra <chris@kingparra.work>
2020-10-29 13:05:57 -0400

    Elaborate on mult

--- 08_-_recursion/exercises/8.6.3_-_recursion.rst.d/multiply/src/Lib.hs
+++ 08_-_recursion/exercises/8.6.3_-_recursion.rst.d/multiply/src/Lib.hs
@@ -1,10 +1,15 @@
 module Lib (mult) where
 
 mult :: Integral a => a -> a -> a
-mult x y =
-  let go n m count
-        | n == 0 = 0
-        | m == 0 = 0
-        | count == abs m = n
-        | otherwise      = go (n+x) m (count+1)
-  in  go x y 1
+mult x y
+  -- moved out of inner function in case
+  -- progressing from a negative number
+  -- to a positive ends evaluation early
+  | x == 0 = 0
+  | y == 0 = 0
+  | otherwise =
+      let go n m count
+            -- negative numbers only work for the first input...
+            | count == abs m = n
+            | otherwise      = go (n+x) m (count+1)
+      in  go x y 1
