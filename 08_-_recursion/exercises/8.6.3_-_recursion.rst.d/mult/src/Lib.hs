6a0a6dde99c96830ad9012089b3c2443e3b28434
Chris King-Parra <chris@kingparra.work>
2020-10-30 21:16:43 -0400

    Add ex 8.6.3.3 multiply function

---
+++ 08_-_recursion/exercises/8.6.3_-_recursion.rst.d/mult/src/Lib.hs
@@ -0,0 +1,20 @@
+module Lib (mult) where
+
+mult :: Integral a => a -> a -> a
+mult x y
+  | x == 0 = 0
+  | y == 0 = 0
+  | otherwise = case signum y of
+      1 -> go x y 1
+      (-1) -> negate $ go x (abs y) 1
+  where 
+    go n m count
+      | count == abs m  =  n
+      | otherwise       =  go (n+x) m (count+1)
+
+--mult :: Integral a => a -> a -> a
+--mult x y = 
+--  if signum y == (-1) 
+--  then  negate . foldr (+) 0 . take (abs y) $ repeat x
+--  else           foldr (+) 0 . take (abs y) $ repeat x
+  
