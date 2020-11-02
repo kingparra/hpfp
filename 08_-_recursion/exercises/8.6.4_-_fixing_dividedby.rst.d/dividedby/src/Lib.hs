31952603f4cb105dfdf3251cb0fb7914019f0f3c
Chris King-Parra <chris@kingparra.work>
2020-11-02 01:47:32 -0500

    Add test case for 8.6.4

--- 08_-_recursion/exercises/8.6.4_-_fixing_dividedby.rst.d/dividedby/src/Lib.hs
+++ 08_-_recursion/exercises/8.6.4_-_fixing_dividedby.rst.d/dividedby/src/Lib.hs
@@ -2,4 +2,9 @@ module Lib (dividedBy', DividedResult(..)) where
 
 data DividedResult = Result Integer | DividedByZero deriving (Eq, Show)
 
-dividedBy' num denom = undefined
+dividedBy' :: Integer -> Integer -> DividedResult
+dividedBy' num denom
+  |                       signum denom == 0      =    DividedByZero
+  |    signum num == 0 && signum denom /= 0      =    Result 0
+  |    signum num == 1 && signum denom == 1      =    Result (num `div` denom)
+  | signum num == (-1) || signum denom == (-1)   =    Result (num `div` denom)
--- 08_-_recursion/exercises/8.6.4_-_fixing_dividedby.rst.d/dividedby/test/Spec.hs
+++ 08_-_recursion/exercises/8.6.4_-_fixing_dividedby.rst.d/dividedby/test/Spec.hs
@@ -1,4 +1,4 @@
-import Test.Hspec (hspec, describe, it, shouldBe)
+import Test.Hspec (hspec, context, describe, it, shouldBe)
 import Lib (dividedBy', DividedResult(..))
 
 -- The main issue we want to address is that
@@ -6,20 +6,16 @@ import Lib (dividedBy', DividedResult(..))
 -- less.
 main :: IO ()
 main = hspec $ do
-
   describe "dividedBy'" $ do
-
     it "10 2 -> Result 5" $ do
       dividedBy' 10 2 `shouldBe` Result 5
-
     it "10 (-2) -> Result (-5)" $ do
       dividedBy' 10 (-2) `shouldBe` Result (-5)
-
     it "(-10) (-2) -> Result 5" $ do
       dividedBy' (-10) (-2) `shouldBe` Result 5
-
     it "(-10) 2 -> Result (-5)" $ do
       dividedBy' (-10) 2 `shouldBe` Result (-5)
-
     it "8 0 -> DividedByZero" $ do
       dividedBy' 8 0 `shouldBe` DividedByZero
+    it "0 8 -> 0" $ do
+      dividedBy' 0 8 `shouldBe` 0
