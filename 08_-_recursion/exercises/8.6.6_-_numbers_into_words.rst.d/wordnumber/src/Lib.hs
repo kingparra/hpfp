b775702c1d87a40552693d653cf0495ad4ce31f5
Chris King-Parra <chris@kingparra.work>
2020-11-03 12:56:53 -0500

    First draft of wordNumber
    This currently does not work for
    inputs with leading zeros in it.

--- 08_-_recursion/exercises/8.6.6_-_numbers_into_words.rst.d/wordnumber/src/Lib.hs
+++ 08_-_recursion/exercises/8.6.6_-_numbers_into_words.rst.d/wordnumber/src/Lib.hs
@@ -2,11 +2,23 @@ module Lib where
 import Data.List (intersperse)
 
 digitToWord :: Int -> String
-digitToWord n = undefined
+digitToWord 0 = "zero"
+digitToWord 1 = "one"
+digitToWord 2 = "two"
+digitToWord 3 = "three"
+digitToWord 4 = "four"
+digitToWord 5 = "five"
+digitToWord 6 = "six"
+digitToWord 7 = "seven"
+digitToWord 8 = "eight"
+digitToWord 9 = "nine"
+digitToWord _ = error "fuck"
 
 digits :: Int -> [Int]
-digits n = undefined
+digits 0 = []
+digits n = digits (n `div` 10) ++ [ n `mod` 10 ]
 
 wordNumber :: Int -> String
-wordNumber n = undefined
-
+wordNumber n = if signum n == (-1)
+  then "negative-" ++ wordNumber (abs n)
+  else concat . intersperse "-" . map digitToWord $ digits n
--- 08_-_recursion/exercises/8.6.6_-_numbers_into_words.rst.d/wordnumber/test/Spec.hs
+++ 08_-_recursion/exercises/8.6.6_-_numbers_into_words.rst.d/wordnumber/test/Spec.hs
@@ -1,25 +1,29 @@
-import hspec (describe, context, it, shouldBe)
-import Lib
+import Test.Hspec (hspec, describe, context, it, shouldBe)
+import Lib (digitToWord, digits, wordNumber)
 
 main = hspec $ do
-  describe "wordNumber" $ do
-    -- wordNumber works only on Int, so we don't have to worry about decimals.
+  describe "digitToWord" $ do
+    -- wordNumber works only on Int, so we don't have to worry about
+    -- decimals.
     it "converts single digits to words without surrounding dashes" $ do
-      wordNumber 0 `shouldBe` "zero"
-      wordNumber 1 `shouldBe` "one"
-      wordNumber 2 `shouldBe` "two"
-      wordNumber 3 `shouldBe` "three"
-      wordNumber 4 `shouldBe` "four"
-      wordNumber 5 `shouldBe` "five"
-      wordNumber 6 `shouldBe` "six"
-      wordNumber 7 `shouldBe` "seven"
-      wordNumber 8 `shouldBe` "eight"
-      wordNumber 9 `shouldBe` "nine"
+      digitToWord 0 `shouldBe` "zero"
+      digitToWord 1 `shouldBe` "one"
+      digitToWord 2 `shouldBe` "two"
+      digitToWord 3 `shouldBe` "three"
+      digitToWord 4 `shouldBe` "four"
+      digitToWord 5 `shouldBe` "five"
+      digitToWord 6 `shouldBe` "six"
+      digitToWord 7 `shouldBe` "seven"
+      digitToWord 8 `shouldBe` "eight"
+      digitToWord 9 `shouldBe` "nine"
+  describe "wordNumber" $ do
     it "intersperses dashes for multi-digit outputs" $ do
       wordNumber 123 `shouldBe` "one-two-three"
       wordNumber 88 `shouldBe` "eight-eight"
+      -- digits currently strips leading zeros, fix this.
       wordNumber 007 `shouldBe` "zero-zero-seven"
       wordNumber 123456 `shouldBe` "one-two-three-two-four-five-four-six"
     it "prefixes 'negative' when the input is a negative number" $ do
       wordNumber (-98) `shouldBe` "negative-nine-eight"
-      -- Since (-0) resolves to 0 before function application, we don't need a special case for it.
+      -- Since (-0) resolves to 0 before function application, we don't
+      -- need a special case for it.
