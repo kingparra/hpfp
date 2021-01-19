497d48dfc36685cd95d97d6b525c01ea458045b8
Chris King-Parra <chris@kingparra.work>
2021-01-19 17:17:02 -0500

    Add imports to Main
    Add imports for functions we'll use
    to Main.
    
    From 13.10 Step One: Importing modules, page 513.

--- 13_-_building_projects/projects/hangman/src/Main.hs
+++ 13_-_building_projects/projects/hangman/src/Main.hs
@@ -1,5 +1,13 @@
 module Main where
 
+import Control.Monad (forever)
+import Data.Char (toLower)
+import Data.Maybe (isJust)
+import Data.List (intersperse)
+import System.Exit (exitSuccess)
+import System.Random (randomRIO)
+
+
 main :: IO ()
 main = do
   putStrLn "hello world"
