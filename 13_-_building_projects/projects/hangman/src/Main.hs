c413413030d0e364a5508ef7949307702a0362e9
Chris King-Parra <chris@kingparra.work>
2021-01-19 22:06:32 -0500

    Add function to split dict.txt into words
    Add a function, allWords, that will read in dict.txt
    and split it into lines for further digestion in our
    program. We also add the WordList type alias here.
    
    From 13.11 Step Two: Generating a word list, page 517,
    paragraphs 1 through 4.

--- 13_-_building_projects/projects/hangman/src/Main.hs
+++ 13_-_building_projects/projects/hangman/src/Main.hs
@@ -11,3 +11,12 @@ import System.Random (randomRIO)
 main :: IO ()
 main = do
   putStrLn "hello world"
+
+
+type WordList = [String]
+
+
+allWords :: IO WordList
+allWords = do
+  dict <- readFile "data/dict.txt"
+  return (lines dict)
