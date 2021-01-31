f432f23fd771b82302e720dbe326f94e29e4b3e9
Chris King-Parra <chris@kingparra.work>
2021-01-30 20:36:12 -0500

    Add quickcheck test to addition

--- 14_-_testing/projects/addition/Addition.hs
+++ 14_-_testing/projects/addition/Addition.hs
@@ -1,14 +1,15 @@
 module Addition where
 import Test.Hspec
+import Test.QuickCheck
 
 
 dividedBy :: Integral a => a -> a -> (a, a)
 dividedBy num denom =
   go 0 num denom
-  where 
-    go count n d = 
-      if n < d 
-      then (count,n) 
+  where
+    go count n d =
+      if n < d
+      then (count,n)
       else go (count+1) (n-d) d
 
 
@@ -18,6 +19,8 @@ main = hspec $ do
   describe "Addition" $ do
     it "1 + 1 is greater than 1" $ do
       (1 + 1) > 1 `shouldBe` True
+    it "x+1 is always greater than x" $ do
+      property $ \x -> x + 1 > (x :: Int)
 
   describe "Division" $ do
     it "15 divided by 3 is  5" $ do
