0aa6e57b90aa7f7de0e0455c5c94d124e9d62565
Chris King-Parra <chris@kingparra.work>
2021-02-03 10:39:47 -0500

    Add functions from pages 551..553

--- 14_-_testing/projects/addition/Addition.hs
+++ 14_-_testing/projects/addition/Addition.hs
@@ -3,6 +3,7 @@ import Test.Hspec
 import Test.QuickCheck
 
 
+
 dividedBy :: Integral a => a -> a -> (a, a)
 dividedBy num denom =
   go 0 num denom
@@ -11,6 +12,65 @@ dividedBy num denom =
       if n < d
       then (count,n)
       else go (count+1) (n-d) d
+
+
+
+-- from page 551
+-- Play with these in the repl by using sample on them.
+genBool :: Gen Bool
+genBool = choose (False,True)
+
+genBool' :: Gen Bool
+genBool' = elements [False,True]
+
+genOrdering :: Gen Ordering
+genOrdering = elements [LT,EQ,GT]
+
+genChar :: Gen Char
+genChar = elements ['a'..'z']
+
+
+
+-- More complex examples.
+genTuple :: (Arbitrary a, Arbitrary b) => Gen (a,b)
+genTuple = do
+  a <- arbitrary
+  b <- arbitrary
+  return (a,b)
+
+genThreeple :: (Arbitrary a, Arbitrary b, Arbitrary c) => Gen (a,b,c)
+genThreeple = do
+  a <- arbitrary
+  b <- arbitrary
+  c <- arbitrary
+  return (a,b,c)
+
+
+
+-- page 553
+genEither :: (Arbitrary a, Arbitrary b) => Gen (Either a b)
+genEither = do
+  a <- arbitrary
+  b <- arbitrary
+  elements [Left a, Right b]
+
+genMaybe :: Arbitrary a => Gen (Maybe a)
+genMaybe = do
+  a <- arbitrary
+  elements [Nothing, Just a]
+
+genMaybe' :: Arbitrary a => Gen (Maybe a)
+genMaybe' = do
+  a <- arbitrary
+  frequency [ (1, return Nothing)
+            , (3, return (Just a))
+            ]
+-- ·∾ :type frequency
+-- frequency :: [(Int, Gen a)] -> Gen a
+-- ·∾ :doc frequency
+-- Chooses one of the given generators, with a weighted
+-- random distribution. The input list must be non-empty.
+
 
 
 main :: IO ()
