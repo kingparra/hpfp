538185f8ce7528eae1d4a11f5583aeaaf6e02795
Chris King-Parra <chris@kingparra.work>
2021-02-18 17:08:04 -0500

    Someone told me that I look like christopher reeve

--- 15_-_monoid_semigroup/exercises/15.15.1_-_semigroup_exercises.rst.d/semigroup-exercises/src/Lib.hs
+++ 15_-_monoid_semigroup/exercises/15.15.1_-_semigroup_exercises.rst.d/semigroup-exercises/src/Lib.hs
@@ -13,16 +13,10 @@ instance Monoid Trivial where
   mempty = Trivial
   mappend = (<>)
 
--- What is arbitrary, and what is it supposed to do?
--- It seems related to QuickCheck?
---
---  The class method arbitrary is the default generator
---  for type a. You can provide a default generator for
---  any other type by declaring an instance of class
---  Arbitrary that implements the arbitrary method. 
---
 
 instance Arbitrary Trivial where
+  -- What does Arbitrary do again?
+  -- What about Gen?
   -- Why is there a "return" in the sample
   -- code here, and what does it do?
   arbitrary = Gen Trivial
