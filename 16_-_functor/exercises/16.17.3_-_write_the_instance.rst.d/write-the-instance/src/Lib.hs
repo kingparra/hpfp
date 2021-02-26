0a07030059ac413e3f04bd3a2f20d00b328588ef
repl.it user <replituser@example.com>
2021-02-26 01:07:21 +0000

    Add instances for Q10 and 11

--- 16_-_functor/exercises/16.17.3_-_write_the_instance.rst.d/write-the-instance/src/Lib.hs
+++ 16_-_functor/exercises/16.17.3_-_write_the_instance.rst.d/write-the-instance/src/Lib.hs
@@ -41,17 +41,32 @@ data K a b = K a
      to be a b'.  So we can have it pretend to be a
      c, and you only pass the function so fmap is happy.  -}
 instance Functor (K a) where
-  fmap _ (K a) = K a 
-
+  fmap f (K a) = K a
+-- (K a) b
+-- fmap 
+-- Prelude> :t K
+-- K :: a -> K a b
+-- Prelude> :kind K
+-- K :: * -> * -> *
 
 -- Question 3
 newtype Flip f a b = Flip (f b a) deriving (Eq, Show)
-
+--                   ^     ^
+--                   |     |
+--                        (\k a b -> b a) b' a'
+--                            [ a := b' ]
+--                        (\k b -> b b') a'
+--                            [ b := a' ]
+--                        (\k -> a' b')
+--                            a' b'
 newtype K' a b = K' a
 
 -- This should remind you of an instance you've written before.
 -- instance Functor (Flip K' a) where
---   fmap = undefined
+-- (Flip (K' a))
+-- (((Fip K') a) b)
+-- fmap :: (x->y) -> (Flip K' a) x -> (Flip K' a) y 
+--  fmap f (Flip (K' b)) = Flip (K' (f b))
 
 
 -- -- Question 4
@@ -81,18 +96,39 @@ newtype K' a b = K' a
 -- data List a = Nil | Cons a (List a)
 
 
--- -- Question 10
--- data GoatLord a 
---   = NoGoat
---   | OneGoat a
---   | MoreGoats (GoatLord a)
---               (GoatLord a)
---               (GoatLord a)
--- -- A VERITABLE HYDRA OF GOATS
+-- Question 10
+data GoatLord a 
+   = NoGoat
+   | OneGoat a
+   | MoreGoats (GoatLord a)
+               (GoatLord a)
+               (GoatLord a)
+-- A VERITABLE HYDRA OF GOATS
 
+{-
+            *
+       /    |     \
+   NoGoat   *    (OneGoad "Larry")
+          / | \
+        No  *  O "Page"
+           ...
+-}
+instance Functor (GoatLord) where
+  fmap f (NoGoat) = NoGoat
+  fmap f (OneGoat a) = OneGoat (f a)
+  fmap f (MoreGoats a b c) =
+   (MoreGoats (fmap f a) (fmap f b) (fmap f c))
+   
+-- Question 11
+data TalkToMe a 
+   = Halt 
+   | Print String a 
+--   Print [Char] a
+   | Read (String -> a)
 
--- -- Question 11
--- data TalkToMe a 
---   = Halt 
---   | Print String a 
---   | Read (String -> a)
+instance Functor TalkToMe where
+-- fmap :: (a->b) -> f a -> f b 
+  fmap f Halt = Halt
+  fmap f (Print s a) = Print s (f a)
+  fmap f (Read g) = Read (f . g)
+  -- [(String, a)]
