c98983b5b1ee578860a5403af5c1872858646980
Chris King-Parra <chris@kingparra.work>
2021-03-23 18:15:04 -0400

    Add project template for 18.4.3.3

---
+++ 18_-_monad/exercises/18.4.3.3_-_either_monad.rst.d/sum-monad/src/Lib.hs
@@ -0,0 +1,19 @@
+module Lib where
+
+
+data Sum a b = First a | Second b deriving (Eq, Show)
+
+
+instance Functor (Sum b) where
+  fmap f (Second b) = Second (f b)
+  fmap _ (First a) = First a
+
+
+instance Applicative (Sum b) where
+  pure = Second
+  (<*>) a f = undefined
+
+
+instance Monad (Sum b) where
+  return = pure
+  (>>=) = undefined
