f5ef233407538872da1c2f638ec7fe0ea07e9d44
Chris King-Parra <chris@kingparra.work>
2021-04-27 12:14:37 -0400

    Add 22.2 fig 1

---
+++ 22_-_reader/figures/22.2/R2D2v1.hs
@@ -0,0 +1,17 @@
+#!/usr/bin/env stack
+{- stack script
+ --resolver lts-16.28
+ --package hspec
+-}
+module R2D2 where
+import Control.Applicative
+
+
+
+-- page 854, figure 1
+boop = (*2)
+doop = (+10)
+
+
+bip :: Integer -> Integer
+bip = boop . doop
