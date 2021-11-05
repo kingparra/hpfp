6fb5ffdd92cbf4b3c3679c7c40d770d0e76b4ea7
Chris King-Parra <chris@kingparra.work>
2021-11-05 15:31:58 -0400

    Add mnemonic comments to 8.6.2 src file

--- 08_-_recursion/exercises/8.6.2_-_reviewing_currying.rst.d/currying/src/Lib.hs
+++ 08_-_recursion/exercises/8.6.2_-_reviewing_currying.rst.d/currying/src/Lib.hs
@@ -1,6 +1,7 @@
 module Lib where
 
 
+-- _(++" mrow "++)_
 cattyConny :: String -> String -> String
 cattyConny x y = x ++ " mrow " ++ y
 
@@ -9,9 +10,11 @@ flippy :: String -> String -> String
 flippy = flip cattyConny
 
 
+-- ("woops mrow"++)_
 appedCatty :: String -> String
 appedCatty = cattyConny "woops"
 
 
+-- _(++" mrow haha")
 frappe :: String -> String
 frappe = flippy "haha"
