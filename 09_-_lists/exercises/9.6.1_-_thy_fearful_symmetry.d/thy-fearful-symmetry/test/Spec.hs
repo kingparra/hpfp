3fdf267782217785f9b0edceef07df09a016059b
Chris King-Parra <chris@kingparra.work>
2022-01-24 18:20:17 -0500

    Add property test for myLines in ex 9.6.1

--- 09_-_lists/exercises/9.6.1_-_thy_fearful_symmetry.d/thy-fearful-symmetry/test/Spec.hs
+++ 09_-_lists/exercises/9.6.1_-_thy_fearful_symmetry.d/thy-fearful-symmetry/test/Spec.hs
@@ -10,10 +10,12 @@ main = hspec $ do
       myWords "sheryl wants fun" `shouldBe` ["sheryl","wants","fun"]
     it "won't split on other whitespace characters" $ do
       myWords "don't\nyou\tknow\vumbrellas are great" `shouldBe` ["don't\nyou\tknow\vumbrellas","are","great"]
-    -- what should myWords do for empty strings? "", or [""], something else?
     it "will respect empty lists" $ do
       myWords "" `shouldBe` []
 
   describe "myLines" $ do
     it "will split a string on newlines" $ do
       myLines sentences `shouldBe` lines sentences
+    it "will produce the same output as lines" $ do
+      property (\x -> myLines x == lines x)
+      -- property $ verboseCheck (\x -> myLines x == lines x)
