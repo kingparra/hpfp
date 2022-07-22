b507cbdba6a7c8f8580e889fcff38a7a5b619326
Chris King-Parra <chris@kingparra.work>
2022-07-22 10:12:57 +0100

    Implement Qs 10 and 11 of ex 10.10.2

--- 10_-_folds/exercises/10.10.2_-_rewriting_functions_using_folds.rst.d/rewrite-with-folds/src/Lib.hs
+++ 10_-_folds/exercises/10.10.2_-_rewriting_functions_using_folds.rst.d/rewrite-with-folds/src/Lib.hs
@@ -34,3 +34,17 @@ squish = foldr (++) []
 
 squishMap :: (a -> [b]) -> [a] -> [b]
 squishMap f = squish . myMap f
+
+
+squishAgain :: [[a]] -> [a]
+squishAgain = squishMap id
+
+
+-- I could reutrn mempty if a had a constraint of Monoid, but according
+-- to this type signature, I have to throw an exception for empty lists.
+myMaximumBy :: (a -> a -> Ordering) -> [a] -> a
+myMaximumBy f = foldl1 (\x y -> case f x y of { GT -> x; _ -> y })
+
+
+myMinimumBy :: (a -> a -> Ordering) -> [a] -> a
+myMinimumBy f = foldl1 (\x y -> case f x y of { LT -> x; _ -> y })
--- 10_-_folds/exercises/10.10.2_-_rewriting_functions_using_folds.rst.d/rewrite-with-folds/test/Spec.hs
+++ 10_-_folds/exercises/10.10.2_-_rewriting_functions_using_folds.rst.d/rewrite-with-folds/test/Spec.hs
@@ -14,17 +14,17 @@ main = hspec $ do
       myAny odd [1,3,5] `shouldBe` True
 
   describe "Question 3: myElem" $ do
-    it "simple test cases" $ do
+    it "simple inputs" $ do
       myElem 1 [1..10] `shouldBe` True
       myElem 1 [2..10] `shouldBe` False
 
   describe "Question 4: myReverse" $ do
-    it "simple test cases" $ do
+    it "simple inputs" $ do
       myReverse "blah" `shouldBe` "halb"
       myReverse [1..5] `shouldBe` [5,4..1]
 
   describe "Question 5: myMap" $ do
-    it "simple test cases" $ do
+    it "simple inputs" $ do
       myMap (+3) [1..3] `shouldBe` map (+3) [1..3]
     prop "myMap :: Int -> [Int] ≡ map :: Int -> [Int]" $ do
       ((\n l -> myMap (*n) l `shouldBe` map (*n) l)
@@ -33,15 +33,37 @@ main = hspec $ do
                 :: Int -> [Int] -> Expectation)
 
   describe "Question 6: myFilter" $ do
-    it "simple test cases" $ do
+    it "simple inputs" $ do
       myFilter (\x -> x == 'a') "thearwhat" `shouldBe` "aa"
 
   describe "Question 7: squish" $ do
-    it "simple test cases" $ do
+    it "simple inputs" $ do
       squish ["this","and","that"] `shouldBe` "thisandthat"
 
   describe "Question 8: squishMap" $ do
-    it "simple test cases" $ do
+    it "simple inputs" $ do
       squishMap (take 3) [[1..], [10..], [100..], [1000..]]
         `shouldBe` [1,2,3,10,11,12,100,101,102,1000,1001,1002]
       squishMap (take 3) [[1..]] `shouldBe` [1,2,3]
+
+  describe "Question 9: squishMap" $ do
+    it "simple inputs" $ do
+      squishMap (take 3) [[1..], [10..], [100..], [1000..]]
+        `shouldBe` [1,2,3,10,11,12,100,101,102,1000,1001,1002]
+      squishMap (take 3) [[1..]] `shouldBe` [1,2,3]
+
+  describe "Question 10: myMaximumBy" $ do
+    it "simple inputs" $ do
+      myMaximumBy (\_ _ -> GT) [1..10] `shouldBe` 1
+      myMaximumBy (\_ _ -> LT) [1..10] `shouldBe` 10
+      myMaximumBy compare [1..10]      `shouldBe` 10
+      myMaximumBy (\x y -> compare (length x) (length y))
+        ["Hello", "World", "!", "Longest", "bar"] `shouldBe` "Longest"
+
+  describe "Question 11: myMinimumBy" $ do
+    it "simple inputs" $ do
+      myMinimumBy (\_ _ -> GT) [1..10] `shouldBe` 10
+      myMinimumBy (\_ _ -> LT) [1..10] `shouldBe` 1
+      myMinimumBy compare [1..10]      `shouldBe` 1
+      myMinimumBy (\x y -> compare (length x) (length y))
+        ["Hello", "World", "!", "Longest", "bar"] `shouldBe` "!"
