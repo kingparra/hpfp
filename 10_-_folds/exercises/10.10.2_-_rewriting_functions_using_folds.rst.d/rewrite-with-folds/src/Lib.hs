591c4e0142e4e6e63e2b2281255f71a84b82d359
Chris King-Parra <chris@kingparra.work>
2022-07-22 10:18:37 +0100

    Include solutions in ex 10.10.2 rst file

--- 10_-_folds/exercises/10.10.2_-_rewriting_functions_using_folds.rst.d/rewrite-with-folds/src/Lib.hs
+++ 10_-_folds/exercises/10.10.2_-_rewriting_functions_using_folds.rst.d/rewrite-with-folds/src/Lib.hs
@@ -1,26 +1,32 @@
 module Lib where
 
 
+-- Question 1
 myOr :: [Bool] -> Bool
 myOr = foldr (||) False
 
 
+-- Question 2
 myAny :: (a -> Bool) -> [a] -> Bool
 myAny f = foldr (\x xs -> f x || xs) False
 
 
+-- Question 3
 myElem :: Eq a => a -> [a] -> Bool
 myElem e = foldr (\x xs -> x == e || xs) False
 
 
+-- Question 4
 myReverse :: [a] -> [a]
 myReverse = foldl (flip (:)) []
 
 
+-- Question 5
 myMap :: (a -> b) -> [a] -> [b]
 myMap f = foldr ((:) . f) []
 
 
+-- Question 6
 myFilter :: (a -> Bool) -> [a] -> [a]
 myFilter f =
   foldr (\x xs -> if f x == True
@@ -28,23 +34,29 @@ myFilter f =
                   else xs) []
 
 
+-- Question 7
 squish :: [[a]] -> [a]
 squish = foldr (++) []
 
 
+-- Question 8
 squishMap :: (a -> [b]) -> [a] -> [b]
 squishMap f = squish . myMap f
 
 
+-- Question 9
 squishAgain :: [[a]] -> [a]
 squishAgain = squishMap id
 
 
--- I could reutrn mempty if a had a constraint of Monoid, but according
--- to this type signature, I have to throw an exception for empty lists.
+-- Question 10
+-- I could reutrn mempty if a had a constraint of Monoid, but
+-- according to this type signature, I have to throw an exception
+-- for empty lists.
 myMaximumBy :: (a -> a -> Ordering) -> [a] -> a
 myMaximumBy f = foldl1 (\x y -> case f x y of { GT -> x; _ -> y })
 
 
+-- Question 11
 myMinimumBy :: (a -> a -> Ordering) -> [a] -> a
 myMinimumBy f = foldl1 (\x y -> case f x y of { LT -> x; _ -> y })
