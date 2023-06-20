2adf7bb2a52dac71f3c5cf41199cd05f909e8224
Chris King-Parra <chris@kingparra.work>
2023-06-20 18:11:10 -0400

    Add a test for mc91 function

--- 08_-_recursion/exercises/8.6.5_-_mccarthy_91_function.rst.d/mc91/test/Spec.hs
+++ 08_-_recursion/exercises/8.6.5_-_mccarthy_91_function.rst.d/mc91/test/Spec.hs
@@ -6,3 +6,5 @@ main = hspec $ do
   describe "mc91" $ do
     it "should work for the example input" $ do
       map mc91 [95..110] `shouldBe` [91,91,91,91,91,91,91,92,93,94,95,96,97,98,99,100]
+    it "should return 91 where n > 1 && n <= 100"$ do
+      map mc91 [1..100] `shouldBe` take 100 (cycle [91])
