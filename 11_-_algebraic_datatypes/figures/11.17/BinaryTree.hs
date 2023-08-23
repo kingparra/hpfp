33b564facb768aca7b88bc5fe152e25efabed218
Chris King-Parra <chris@kingparra.work>
2023-08-23 10:55:07 -0400

    Remove existing notes and exercises for ch11

--- 11_-_algebraic_datatypes/figures/11.17/BinaryTree.hs
+++
@@ -1,5 +0,0 @@
--- page 446
-data BinaryTree a
-  = Leaf
-  | Node (BinaryTee a) a (BinaryTree a)
-  deriving (Eq, Ord, Show)
--- 11_-_algebraic_datatypes/figures/11.17/Insert.hs
+++
@@ -1,7 +0,0 @@
--- page 447
-insert' :: Ord a => a -> BinaryTree a -> BinaryTree a
-insert' b Leaf = Node Leaf b Leaf
-insert' b (Node left a right)
-  | b == a  =  Node left a right
-  | b < a   =  Node (insert' b left) a right
-  | b > a   =  Node left a (insert' b right)
