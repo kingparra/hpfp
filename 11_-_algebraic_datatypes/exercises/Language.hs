1193badd294f55ec26e843ac24217a3619086fb2
Chris King-Parra <chris@kingparra.work>
2023-10-08 16:17:39 -0400

    Fix trailing period bug

--- 11_-_algebraic_datatypes/exercises/Language.hs
+++ 11_-_algebraic_datatypes/exercises/Language.hs
@@ -1,24 +1,25 @@
 #!/usr/bin/env stack
--- stack --resolver lts-20.18 script --package hspec
-import Data.List.Split (splitOn)
+-- stack --resolver lts-20.18 script --package hspec --package text
 import Data.List (intersperse)
 import Data.Char (toUpper)
 import Test.Hspec
 
 
 -- 1. Write a function that capitalizes a word.
-capitalizeWord l@(x:xs) = toUpper x : xs
-
+capWord l@(x:xs) = toUpper x : xs
 
 -- 2. Write a function that capitalizes sentences in a paragraph.
 -- Recognize when a new sentence has begun by checking for periods.
-capitalizeParagraph p =
-  let sentences = map (++".") (splitOn ". " p)
-  in concat $ intersperse " " $ map (\(x:xs) -> toUpper x : xs) sentences
+splitPara p
+  | p == "" = []
+  | otherwise = 
+     [takeWhile (/='.') p ++ "."] ++ splitPara (drop 2 $ dropWhile (/='.') p)
+
+capPara p = concat $ intersperse " " $ map capWord $ splitPara p
 
 
 main :: IO ()
 main = hspec $ do
   describe "capitalizeParagraph" $ do
-    it "returns the first element of a list" $ do
-      capitalizeParagraph "blah. woot ha" `shouldBe` "Blah. Woot ha."
+    it "Capitalizes da paragraph, dude!" $ do
+      capPara "blah. woot ha." `shouldBe` "Blah. Woot ha."
